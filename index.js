6f63b8c780d3247cd4aac07499877156ac17ee62
Kayo Galdino <kayo.kg@hotmail.com>
2023-05-13 15:47:28 -0300

    Parte simples do jogo finalizada

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function armazenarValor() {
-    var valorInput = document.getElementById("myInput").value;
-    localStorage.setItem("inputValor", valorInput);
-    window.location.href = "game.html";
+const input = document.querySelector(".myInput");
+const form = document.querySelector(".resultadoForm");
+
+const capturarNome = (evento) => {
+  console.log("capturando nome");
+  localStorage.setItem("nome", input.value);
+  window.location = "game.html";
 }
+
+form.addEventListener("submit", capturarNome);
